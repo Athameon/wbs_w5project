f99325601282e7cd128d241d8ef31bf4b433ce7e
Joachim0211 <joachims0509@googlemail.de>
2021-05-27 19:23:29 +0200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  // const cards = [{ title: firstTitle, text: firstText }, { title: secondTitle, text: secondText }, { title: thirdTitle, text: thirdText },
+  // { title: fourthTitle, text: fourthText }, { title: fifthTitle, text: fifthText }, { title: sixthTitle, text: sixthText }]
+
   return (
     <div className="App">
-      <h2>Main page</h2>
+
+      <h2>Test</h2>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </div>
   );
 }
