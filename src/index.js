6919f483e8b7c86fccf119deb46bf033de5667ff
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-05-28 15:59:57 +0200

    implemented fetch

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Header from './Header'
 import Footer from './Footer'
 
 function Main() {
+  const addressingApi = "http://hn.algolia.com/api/v1/"
   const [searchObject, setSearchObject] = useState(
     { 
       query: "",
@@ -13,13 +14,61 @@ function Main() {
       time: {id: "all", value: "All time"},
       order: "Date"
     })
+  const [searchResult, setSearchResult] = useState();
 
   useEffect(() => {
     performFetch();
   }, [searchObject])
 
   const performFetch = () => {
-    console.log("Fetch new data", searchObject)
+    console.log("Fetch new data with search object: ", searchObject);
+    let address = searchObject.order === "Date"?
+      addressingApi + "search_by_date?" : 
+      addressingApi + "search?";
+    console.log(searchObject.search)
+    if(searchObject.search === "Stories") {
+      address += 'tags=story&';
+    } else if (searchObject.search === "Comments") {
+      address += 'tags=comment&';
+    }
+    if (searchObject.time.id !== 'all') {
+      address += "numericFilters=created_at_i>" + getTimeRange(searchObject.time)+'&';
+    }
+    if (searchObject.query !== '') {
+      address += "query=" + searchObject.query;
+    }
+    console.log('searchAdress: ', address);
+
+    fetch(address)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request was too stupid!');
+    }, networkError => {
+      console.log(networkError.message);
+    })
+    .then(jsonResponse => {
+      console.log(jsonResponse);
+      setSearchResult(jsonResponse);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  const getTimeRange = (time) => {
+    switch(time.id) {
+      case "day":
+        return 24*60*60;
+      case "week":
+        return 7*24*60*60;
+      case "month":
+        return 31*24*60*60;
+      case "year":
+        return 365*24*60*60;
+      default: 
+        return "";
+    }
   }
 
   const inputFilterQuerySet = (searchQueryValue) => {
