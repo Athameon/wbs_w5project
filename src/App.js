6f9a8cd234c3685b90d70091fb822e0dabf58c18
Joachim0211 <joachims0509@googlemail.de>
2021-05-28 15:26:31 +0200

    theCards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import Cards from './Cards';
 
 function App() {
 
-  // const cards = [{ title: firstTitle, text: firstText }, { title: secondTitle, text: secondText }, { title: thirdTitle, text: thirdText },
-  // { title: fourthTitle, text: fourthText }, { title: fifthTitle, text: fifthText }, { title: sixthTitle, text: sixthText }]
+  const cards = [{ title: 'first', text: 'firstText' }, { title: 'secondTitle', text: 'secondText' }, { title: 'thirdTitle', text: 'thirdText' },
+  { title: 'fourthTitle', text: 'fourthText' }, { title: 'fifthTitle', text: 'fifthText' }, { title: 'sixthTitle', text: 'sixthText' }]
 
   return (
     <div className="App">
 
-      <h2>Test</h2>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+      <h2>{cards.map(() => <Cards /> )}</h2>
+      
     </div>
   );
 }
