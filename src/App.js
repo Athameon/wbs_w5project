a926d3cea86e3faee26f1b9a9fc9c7128aef8d41
Athameon <Benjamin.Nopper@freenet.de>
2021-05-28 19:12:26 +0200

    Added to display the comment if it is a comment and not a story. Added display of the time of the element

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './App.css';
 import Card from './Card';
 
 function App({searchResult}) {
+
   if (searchResult === undefined) { // In case the result is not loaded yet
     return <div></div>
   }
+
+  searchResult.hits = searchResult.hits.sort(function(a,b){
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return new Date(b.created_at) - new Date(a.created_at);
+  });
 
   return (
     <div className="container">
--- src/Card.js
+++ src/Card.js
@@ -5,8 +5,9 @@ export default ({card}) => {
   return (
     <div className="card col-l" style={{width: '28rem'}}>
         <div className="card-body ">
-          <h5 className="card-title">{card.title}</h5>
+          <h5 className="card-title">{card.title? card.title : card.comment_text}</h5>
           <p className="card-text">{card.author}</p>
+          <p className="card-text">{new Date(card.created_at).toLocaleString()}</p>
         </div>
     </div>
   )
