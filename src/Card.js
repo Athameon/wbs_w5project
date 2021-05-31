53b53c5ca1894eebd4fb666b68e56f2a596dc22d
Athameon <Benjamin.Nopper@freenet.de>
2021-05-31 15:58:52 +0200

    Implemented article link to cards

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./index.js"
+import "./Card.css"
 
 export default ({card}) => {
   return (
     <div className="card col-l" style={{width: '50%'}}>
         <div className="card-body ">
-          <h5 className="card-title">{card.title? card.title : card.comment_text}</h5>
+          <a href={card.url} target="_blank" className="card-title">{card.title? card.title : card.comment_text}</a>
           <p className="card-text">{card.author}</p>
           <p className="card-text">{new Date(card.created_at).toLocaleString()}</p>
         </div>
