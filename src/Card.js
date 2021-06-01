fe4fb2dd40e995b6634b36bafb16f2739f1916dc
Jugesh <jugeshkumar0208@gmail.com>
2021-06-01 17:17:22 +0200

    added-comment

--- src/Card.js
+++ src/Card.js
@@ -6,13 +6,22 @@ const Card = ({ card }) => {
   return (
     <div className="card col-l" style={{ width: "50%" }}>
       <div className="card-body ">
-        <a href={card.url} target="_blank" rel="noreferrer" className="card-title">
+        <a
+          href={card.url}
+          target="_blank"
+          rel="noreferrer"
+          className="card-title"
+        >
           {card.title ? card.title : card.comment_text}
         </a>
-        <p className="card-text">{card.author}</p>
         <p className="card-text">
           {new Date(card.created_at).toLocaleString()}
         </p>
+        <p>
+          {card.points ? card.points : 0} Points | {card.author} |{" "}
+          {card.num_comments ? card.num_comments : 0} comments
+        </p>
+        <p>{}</p>
       </div>
     </div>
   );
